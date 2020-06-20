8f6d4dcbba390a3573dbb5d26d0d4b764d6bc117
margin25 <55027213+margin25@users.noreply.github.com>
2020-06-20 11:41:46 -0600

    update mentors.dart

--- mentor_her/lib/home/mentors.dart
+++ mentor_her/lib/home/mentors.dart
@@ -3,9 +3,103 @@ import 'package:flutter/material.dart';
 class Mentors extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Column(
+
+    return Center(
+      child: RaisedButton(
+        onPressed: (){
+          print('you clicked me');
+          navigateToSubPage(context);
+        },
+        child: Text('Find Mentors',style: TextStyle(fontSize: 20 )),
+        color: Colors.red,
+          textColor: Colors.white,
+        shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10)),
+
       ),
     );
+
+  }
+  Future navigateToSubPage(context) async {
+    Navigator.push(context, MaterialPageRoute(builder: (context) => Subpage()));
+  }
+}
+
+void mentor() {
+
+  runApp(
+      MaterialApp(
+        title: "Stateful",
+        home: Subpage(),
+      )
+  );
+}
+
+class Subpage extends StatefulWidget {
+
+  @override
+  State<StatefulWidget> createState() {
+    return _SubpageState();
+  }
+}
+
+class _SubpageState extends State<Subpage> {
+
+  String nameCity = "";
+  var _currencies = ['Select Category Here','Finance', 'Technology', 'Marketing', 'Human Resources','Entrepreneurship'];
+  var _currentItemSelected = 'Select Category Here';
+
+  @override
+  Widget build(BuildContext context) {
+
+    debugPrint("Favorite City widget is created");
+
+    return Scaffold(
+      appBar: AppBar(
+        iconTheme: IconThemeData(
+            color: Colors.white
+        ),
+        title: const Text('Add Mentors', style: TextStyle(
+            color: Colors.white
+        )),
+        backgroundColor: Colors.red,
+      ),
+      body: Container(
+        margin: EdgeInsets.all(20.0),
+        child: Column(
+          children: <Widget>[
+            TextField(
+              onSubmitted: (String userInput) {
+                setState(() {
+                  debugPrint("set State is called, this tells framework to redraw the Subpage widget");
+                  nameCity = userInput;
+                });
+              },
+            ),
+            DropdownButton<String>(
+              items: _currencies.map((String dropDownStringItem) {
+                return DropdownMenuItem<String>(
+                  value: dropDownStringItem,
+                  child: Text(dropDownStringItem),
+                );
+              }).toList(),
+
+              onChanged: (String newValueSelected) {
+                // Your code to execute, when a menu item is selected from drop down
+                _onDropDownItemSelected(newValueSelected);
+              },
+
+              value: _currentItemSelected,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void _onDropDownItemSelected(String newValueSelected) {
+    setState(() {
+      this._currentItemSelected = newValueSelected;
+    });
   }
 }
